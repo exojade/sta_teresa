3feaf05937f3d3e8dec04c6dd073c1b02df1fba3
Honey Lou L. Danila <itsection.panabo@gmail.com>
2023-05-29 21:07:32 +0800

    first commit

--- includes/constants.php
+++ includes/constants.php
@@ -1,8 +1,9 @@
 <?php
+
     define("DATABASE", "sta_teresa");
     define("USERNAME", "root");
     define("PASSWORD", "");
-    define("PORT", "3306");
+    define("PORT", "3308");
     define("HOST", "localhost");
-    define("SERVER", "localhost:3306");
+    define("SERVER", "localhost:3308");
 ?>
--- public/dashboard_system/main.php
+++ public/dashboard_system/main.php
@@ -1,6 +1,6 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] === "POST") {
-		
+		dump("comment");
 		
     }
 	else {
